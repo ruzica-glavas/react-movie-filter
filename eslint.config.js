97e2baa363b53a68091435bf3deefa9a8f465d5a
ruzica-glavas <ruzi99@hotmail.it>
2025-02-25 15:07:56 +0100

    add: select and list

--- eslint.config.js
+++ eslint.config.js
@@ -33,6 +33,7 @@ export default [
         'warn',
         { allowConstantExport: true },
       ],
+      'react/prop-types': 'off', // Disabilita la regola dei prop types
     },
   },
 ]
--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,43 @@
 import { useState, useEffect } from 'react'
 
+const movies=  [
+  { title: 'Inception', genre: 'Fantascienza' },
+  { title: 'Il Padrino', genre: 'Thriller' },
+  { title: 'Titanic', genre: 'Romantico' },
+  { title: 'Batman', genre: 'Azione' },
+  { title: 'Interstellar', genre: 'Fantascienza' },
+  { title: 'Pulp Fiction', genre: 'Thriller' },
+]
 
 
 function App() {
- const movies=  [
-        { title: 'Inception', genre: 'Fantascienza' },
-        { title: 'Il Padrino', genre: 'Thriller' },
-        { title: 'Titanic', genre: 'Romantico' },
-        { title: 'Batman', genre: 'Azione' },
-        { title: 'Interstellar', genre: 'Fantascienza' },
-        { title: 'Pulp Fiction', genre: 'Thriller' },
-  ]
+
+  const [selectedGenres, setSelectedGenres] = useState (``);
+  const [filteredGenres, setFilteredGenres] = useState(movies);
+
+  
 
   return (
     <>
       <h1>Seleziona il tuo film!</h1>
 
-    
+      <label>Scegli il tuo genere di film:</label>
+
+          <select value={selectedGenres} 
+            onChange={(e) => setSelectedGenres(e.target.value)}>
+            {movies.map ((movie, index)=>
+              <option key={index}>{(movie.genre)}</option>
+            )}
+              
+          </select>
+
+          <ul>
+            {movies.map ((movie, index) => (
+              <li key={index}>{movie.title} {movie.genre}</li>
+            ))
+
+            }
+          </ul>
       
     </>
   )
